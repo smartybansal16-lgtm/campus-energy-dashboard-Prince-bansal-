d8d1b0cf2c6a7665ad8e09d145c6ab6f6998c322
smartybansal16-lgtm <smartybansal16@gmail.com>
2025-12-05 18:12:11 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,10 +2,10 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from src.ingest import load_energy_data
-from src.aggregate import calculate_daily_totals, calculate_weekly_aggregates, building_wise_summary
-from src.visualize import generate_dashboard
-from src.summary import export_outputs
+from code.ingest import load_energy_data
+from code.aggregate import calculate_daily_totals, calculate_weekly_aggregates, building_wise_summary
+from code.visualize import generate_dashboard
+from code.summary import export_outputs
 
 #  Load data
 df = load_energy_data("data")
@@ -22,3 +22,4 @@ generate_dashboard(daily, weekly, summary)
 export_outputs(df, daily, weekly, summary)
 
 print("Pipeline completed successfully!")
+
